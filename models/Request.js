b924ec43f098aee46b0cfa17c63dc095b006d92f
Vítor Resing Plentz <vresingplentz@gmail.com>
2018-12-06 13:45:18 -0200

    Enviando id_solicitacao

--- models/Request.js
+++ models/Request.js
@@ -3,20 +3,20 @@ var Request={
     //STUDENT
     //GETS
     getAllStudentRequests:function(matricula, callback){
-      return db.query(" select AL.nome_aluno, S.data_solic, S.horas_info, S.pdf, A.nome_atividade, C.status, C.data_correcao, C.horas_aceitas,"+
+      return db.query(" select AL.nome_aluno, S.id_solicitacao, S.data_solic, S.horas_info, S.pdf, A.nome_atividade, C.status, C.data_correcao, C.horas_aceitas,"+
       " C.resp_correcao, CA.nome_categoria from Solicitacao as S, Atividade as A, Correcao as C, Categoria as CA , Aluno as AL "+
       "where S.id_atividade = A.id_atividade and S.id_correcao = C.id_correcao and S.matricula = ? and"+
-      " CA.id_categoria = A.id_categoria and S.matricula = AL.matricula union select AL.nome_aluno, S.data_solic, S.horas_info, S.pdf, "+
+      " CA.id_categoria = A.id_categoria and S.matricula = AL.matricula union select AL.nome_aluno, S.id_solicitacao, S.data_solic, S.horas_info, S.pdf, "+
       "A.nome_atividade, null as status, null as data_correcao, null as horas_aceitas, "+
       "null as resp_correcao, CA.nome_categoria from  Aluno as AL, Solicitacao as S, Atividade as A, Categoria as CA where A.id_atividade = S.id_atividade"+
       " and S.id_correcao is null and S.matricula = ? and CA.id_categoria = A.id_categoria and AL.matricula = S.matricula", [matricula, matricula], callback);
     },
 
     getAllRequests:function(callback){
-      return db.query(" select AL.nome_aluno, AL.matricula, S.data_solic, S.horas_info, S.pdf, A.nome_atividade, C.status, C.data_correcao, C.horas_aceitas,"+
+      return db.query(" select AL.nome_aluno, AL.matricula, S.id_solicitacao, S.data_solic, S.horas_info, S.pdf, A.nome_atividade, C.status, C.data_correcao, C.horas_aceitas,"+
       " C.resp_correcao, CA.nome_categoria from Aluno as AL, Solicitacao as S, Atividade as A, Correcao as C, Categoria as CA "+
       "where AL.matricula = S.matricula and S.id_atividade = A.id_atividade and S.id_correcao = C.id_correcao and"+
-      " CA.id_categoria = A.id_categoria union select AL.nome_aluno, AL.matricula, S.data_solic, S.horas_info, S.pdf, "+
+      " CA.id_categoria = A.id_categoria union select AL.nome_aluno, AL.matricula, S.id_solicitacao, S.data_solic, S.horas_info, S.pdf, "+
       "A.nome_atividade, null as status, null as data_correcao, null as horas_aceitas, "+
       "null as resp_correcao, CA.nome_categoria from  Aluno as AL, Solicitacao as S, Atividade as A, Categoria as CA where  AL.matricula = S.matricula and A.id_atividade = S.id_atividade"+
       " and S.id_correcao is null and CA.id_categoria = A.id_categoria", null, callback);
